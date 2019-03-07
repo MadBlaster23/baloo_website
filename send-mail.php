f507075812f4df51d6bcf136e20912d6cc4ac523
evgenii.k <evgenii.k@prodege.com>
2019-03-07 13:55:33 -0800

    Updates

--- send-mail.php
+++ send-mail.php
@@ -73,6 +73,6 @@ elseif ( isInjected($firstname)
 // If we passed all previous tests, send the email then redirect to the thank you page
 else {
 	mail( "$admin", "Requested date: ".$date, $msg, "From: ".$email);
-	echo "All Data Submitted Successfully!";
+	header( "Location: $thankyou_page" );
 }
 ?>
